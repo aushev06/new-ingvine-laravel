420f8919c1e0b605d73c03fb897af4df34b54678
aushev06 <34713423+aushev06@users.noreply.github.com>
2023-03-29 05:56:12 +0300

    Update TelegramService.php

--- app/Services/Telegram/TelegramService.php
+++ app/Services/Telegram/TelegramService.php
@@ -14,14 +14,19 @@ class TelegramService
     public function sendToTelegram(Order $order, array $properties)
     {
         $setting = Cache::get('settings');
-        $message = "Новый заказ на сайте. Ссылка на заказ - " . 'http://Vanilla Cake.ru/order/' . $order['id'];
-        $chatId  = $setting[Setting::SETTING_TELEGRAM_CHAT_ID]['value'];
-        $botId   = $setting[Setting::SETTING_TELEGRAM_BOT_ID]['value'];
-        $client  = new Client([
+        $message = "Новый заказ на сайте. Ссылка на заказ - " . 'http://dostavka-vanilla.ru/order/' . $order['id'];
+        $chatId = $setting[Setting::SETTING_TELEGRAM_CHAT_ID]['value'];
+        $botId = $setting[Setting::SETTING_TELEGRAM_BOT_ID]['value'];
+        $client = new Client([
             'http_errors' => false
         ]);
-        Log::info('tes', [$chatId, $botId]);
-//        $response = $client->get('https://api.telegram.org/' . $botId . '/sendMessage?chat_id=' . $chatId . '&text=' . $message);
-
+        try {
+            $client->get(
+                'https://api.telegram.org/' . $botId . '/sendMessage?chat_id=' . $chatId . '&text=' . $message
+            );
+        } catch (\Throwable $exception) {
+            Log::error($exception->getMessage());
+            Log::error($exception->getTraceAsString());
+        }
     }
 }
