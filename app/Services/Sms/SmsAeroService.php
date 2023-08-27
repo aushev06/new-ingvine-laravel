d76a4ea73a6a3166e30d68decb9b196253e9ddf8
aushev06 <aushevibra@yandex.ru>
2023-08-27 17:26:26 +0400

    Update SmsAeroService.php

--- app/Services/Sms/SmsAeroService.php
+++ app/Services/Sms/SmsAeroService.php
@@ -26,6 +26,8 @@ class SmsAeroService implements SmsServiceInterface
             return false;
         }
 
+        dd($settings);
+
         $login = $settings['SMS_AERO_LOGIN'];
         $token = $settings['SMS_AERO_TOKEN'];
         $sign = $settings['SMS_AERO_SIGN'];
