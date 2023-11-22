96062a6319a7475c177b13b5bb732a7d51b00fdd
aushev06 <aushevibra@yandex.ru>
2023-11-22 19:14:25 +0300

    fixes

--- resources/js/Pages/SuccessPage.jsx
+++ resources/js/Pages/SuccessPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Head} from '@inertiajs/inertia-react';
 import { Result } from '@/src/components/Result'
 import {MainLayout} from "@/src/layouts/MainLayout";
+import {getCartAsync} from "@/src/features/cart/cartSlice";
+import {useDispatch} from "react-redux";
 export default function SuccessPage(props) {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        localStorage.setItem('token', (new Date().getTime().toString()));
+        dispatch(getCartAsync())
+    }, [])
+
     return (
         <>
             <Head title={'Оплата'}/>
